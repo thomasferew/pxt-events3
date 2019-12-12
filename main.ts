a27b359838b69084a2c348349bf85b825aea4382
thomasferew <58610700+thomasferew@users.noreply.github.com>
2019-12-12 13:44:08 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+let randomNumber15 = Math.randomRange(1, 5)
+game.splash("this is a number 1-5" + ":" + randomNumber15, "")
+let randomNumber610 = Math.randomRange(6, 10)
+game.splash("this is a number 6-10" + ":" + randomNumber610, "")
